74375efbebc52f4fa8ae0471a47c87a2429ffe44
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2021-02-20 20:53:12 +0700

    backend work: get migration to work

---
+++ backend/migrations/20210220103743-add-unique-constraint-for-chatroom-name.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addConstraint("ChatRooms", {
+      type: "unique",
+      fields: ['name'],
+      name: "unique_name"
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeConstraint("ChatRooms", "unique_name");
+  }
+};
