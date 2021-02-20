eb52948ddf0428e687bf5fe1ce040880aa01e9e7
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2021-02-20 21:13:46 +0700

    Added backend routes

---
+++ backend/routes/chatRoom.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const models = requirer('../models');
+const router = express.Router();
+
+/* GET users listing */
+router.get('/chatrooms', async(req, res, next) => {
+    const chatRooms = await models.ChatRoom.findAll();
+    res.send(chatRooms);
+});
+
+router.post('/chatroom', async(req, res, next) => {
+    const room = req.body.room;
+    const chatRooms = await models.ChatRoom.findAll({
+        where: {name: room}
+    });
+    const chatRoom = chatRooms[0];
+    if (!chatRoom) {
+        await models.ChatRoom.create({ name: room });
+    }
+    res.send(chatRooms);
+});
+
+router.get('/chatroom/messages/:chatRoomName', async(req, res, next) => {
+    try {
+        const chatRoomName = req.params.chatRoomName;
+        const chatRooms = await models.ChatRoom.findAll({
+            where: {name: chatRoomName}
+        });
+        const chatRoomId = chatRooms[0].id;
+        const messages = await models.ChatMessage.findAll({
+            where: {chatRoomId}
+        });
+        res.send(messages);
+    } catch (error) {
+        res.send([]);
+    }
+});
+module.exports = router;
