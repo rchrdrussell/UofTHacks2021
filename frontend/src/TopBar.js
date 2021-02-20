c3a1a9ea26a61862fe3c6c459fbc2edd1e878d7e
ryanjgani <ryanscar9@gmail.com>
2021-02-21 02:03:12 +0800

    Created TopBar.js

---
+++ frontend/src/TopBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "react-boostrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { withRouter } from "react-router-dom";
+
+function TopBar({ location }) {
+    const { pathname } = location;
+
+    return (
+        <Navbar bg="primary" expland="lg" variant="dark">
+            <Navbar.Brand href="#home">Chat Room App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/" active ={pathname == "/"}>
+                        Join Another Chat Room
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default withRouter(TopBar);
