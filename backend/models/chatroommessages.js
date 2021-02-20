06237a1c9e664bc4ff0225ad3fe9332ac1e9ebd3
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2021-02-20 21:06:07 +0700

    Create a has many relationship between ChatRoom and ChatRoomMessages

--- backend/models/chatroommessages.js
+++ backend/models/chatroommessages.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 const {
   Model
 } = require('sequelize');
@@ -8,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
-     */
+     
     static associate(models) {
       // define association here
     }
@@ -23,3 +24,24 @@ module.exports = (sequelize, DataTypes) => {
   });
   return ChatRoomMessages;
 };
+*/
+
+/**
+ * Create a has many relationship between ChatRoom and ChatRoomMessages
+ */
+module.exports = (sequelize, DataTypes) => {
+  const ChatMessage = sequelize.define('ChatMessage', {
+    chatRoomId: DataTypes.INTEGER,
+    author: DataTypes.STRING,
+    message: DataTypes.TEXT
+  }, {});
+  ChatMessage.associate = function(models) {
+    // associations can be defined here
+    ChatMessage.belongsTo(models.ChatRoom, {
+      foreignKey: 'chatRoomId',
+      targetKey: 'id'
+    });
+  };
+
+  return ChatMessage
+}
